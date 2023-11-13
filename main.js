3ae16132faa6fdc737600d63209050003aa8b688
lordevito <lollo.devito@hotmail.com>
2023-11-13 20:16:08 +0100

    feat; esercizio svolto

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
 `use strict`;
 
+const lista = document.querySelector(`.lista`);
+const spesa = [`pane`, `pasta`, `gamberi`, `pomodoro`, `pepe`, `basilico`];
+let i = 0;
+
+while (i < spesa.length){
+    const prodotti = document.createElement(`li`);
+    prodotti.classList.add(`prodotti`);
+    prodotti.innerHTML = spesa[i];
+    lista.append(prodotti);
+    console.log(spesa[i]);
+    i++;
+}
+
